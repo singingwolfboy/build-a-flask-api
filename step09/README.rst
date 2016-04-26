5ecbf529995d0197badae2acb37446c5db84b5af
David Baumgold <david@davidbaumgold.com>
2016-04-26 16:50:36 -0400

    Started on Step 9: Marshmallow

---
+++ step09/README.rst
@@ -0,0 +1,12 @@
+Step 9: Transforming Data with Marshmallow
+==========================================
+
+Once again, we've reached a point where it's useful to separate the different
+parts of our code into different places.
+
+
+.. _request: http://flask.pocoo.org/docs/0.10/api/#flask.request
+.. _slugify module: https://github.com/un33k/python-slugify
+.. _SQLAlchemy: http://www.sqlalchemy.org/
+.. _url_for(): http://flask.pocoo.org/docs/0.10/api/#flask.url_for
+.. _curl: https://curl.haxx.se/
---
+++ step09/puppy.py
@@ -0,0 +1,54 @@
+import sys
+from flask import Flask, jsonify, request, url_for
+from models import db, Puppy
+from schemas import ma, puppy_schema
+from slugify import slugify
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///puppy.db"
+db.init_app(app)
+ma.init_app(app)
+
+@app.route("/<slug>")
+def get_puppy(slug):
+    puppy = Puppy.query.filter(Puppy.slug==slug).first_or_404()
+    return puppy_schema.jsonify(puppy)
+
+@app.route("/", methods=["POST"])
+def create_puppy():
+    puppy, errors = puppy_schema.load(request.form)
+    if errors:
+        resp = jsonify(errors)
+        resp.status_code = 400
+        return resp
+
+    puppy.slug = slugify(puppy.name)
+    db.session.add(puppy)
+    db.session.commit()
+
+    # return HTTP response
+    location = url_for("get_puppy", slug=puppy.slug)
+    resp = jsonify({"message": "created"})
+    resp.status_code = 201
+    resp.headers["Location"] = location
+    return resp
+
+if __name__ == "__main__":
+    if "createdb" in sys.argv:
+        with app.app_context():
+            db.create_all()
+        print("Database created!")
+
+    elif "seeddb" in sys.argv:
+        with app.app_context():
+            p1 = Puppy(slug="rover", name="Rover",
+                       image_url="http://example.com/rover.jpg")
+            db.session.add(p1)
+            p2 = Puppy(slug="spot", name="Spot",
+                       image_url="http://example.com/spot.jpg")
+            db.session.add(p2)
+            db.session.commit()
+        print("Database seeded!")
+
+    else:
+        app.run(debug=True)
